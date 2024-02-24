84bfff6ddb934964fb89a5f32ede5da7d4291d6a
AknalRe <94711128+AknalRe@users.noreply.github.com>
2024-02-24 13:24:59 +0700

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,15 @@
+var config = {
+
+loginvc : "Masukkan Kode Tamu/Voucher/Nomor Hp kemudian klik Connect.",
+loginup : "Masukkan Username dan Password <br> kemudian klik Connect.",
+voucherCode : "Kode Tamu/Voucher/Nomor Hp",
+setCase : "none", // lowercase, uppercase or none
+defaultMode : "voucher", // voucher or member
+theme : "default", // default, dark, lite
+
+// status expire
+
+url : "https://demo.mikhmon.online", // url server Mikhmon
+SessionName : "demo", 
+
+}
