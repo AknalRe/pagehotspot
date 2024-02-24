ac49b0f5b46cae28d3685baec9b2b737c0391f1a
AknalRe <94711128+AknalRe@users.noreply.github.com>
2024-02-24 13:32:49 +0700

    Updateup

---
+++ HotspotClarice/banner.js
@@ -0,0 +1,10 @@
+var banner = [
+    { "id": 1, "alt": "CabangBaru", "link": "https://www.claricebeauty.co.id/wp-content/uploads/2024/02/SEGERA-DIBUKA-CABANG-DRIYO-1024x512.jpg"},
+    { "id": 2, "alt": "Ber2-bayar1", "link": "https://www.claricebeauty.co.id/wp-content/uploads/2024/02/ber2-bayar1-1024x512.jpg"},
+    { "id": 3, "alt": "DiskonCabangKlampis", "link": "https://www.claricebeauty.co.id/wp-content/uploads/2024/02/Untitled-1-1024x512.jpg"},
+    { "id": 4, "alt": "BanggaLokal", "link": "https://www.claricebeauty.co.id/wp-content/uploads/2024/02/BCA-1024x512.jpg"},
+    { "id": 5, "alt": "", "link": ""},
+    { "id": 6, "alt": "", "link": ""},
+    { "id": 7, "alt": "", "link": ""},
+    { "id": 8, "alt": "", "link": ""}
+];
